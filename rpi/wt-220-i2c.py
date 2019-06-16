23d7a0212058112fea38f8e5c472f5b690de613f
John Whittington <git@jbrengineering.co.uk>
2019-06-16 18:02:14 +0100

    i2c monitor script and service

---
+++ rpi/wt-220-i2c.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import time
+import RPi.GPIO as GPIO
+import smbus2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.OUT)
+
+ADDR = 0x12 # bus address
+BUS = smbus2.SMBus(1) # indicates /dev/ic2-1
+
+def set_case_led_serial_activity(set):
+    GPIO.output(27, not set)
+
+set_case_led_serial_activity(True)
+
+while (1):
+    try:
+        reply = BUS.read_byte_data(ADDR, 0x03)
+        # print('Status reply:', reply)
+        time.sleep(0.1)
+        shutdown_requested = BUS.read_byte_data(ADDR, 0x02)
+        # print('Shutdown request status:', shutdown_requested)
+        time.sleep(0.1)
+        if (shutdown_requested is 0x01):
+            print("Shutdown request recieved, shutting down")
+            BUS.write_byte(ADDR, 0x01)
+            # os.system("sudo shutdown -h now") 
+        time.sleep(5)
+    except IOError:
+        print("I2C Fail!")
